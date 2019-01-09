5e8ea17e488432ee6d57dedf499802c33ed584bb
paulorbsfilho <p4ulinho@outlook.com>
2019-01-09 09:16:59 -0300

    Autenticação

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {Headers, RequestOptions, Response, Http, RequestMethod} from '@angular/http';
+import {Observable, throwError as observableThrowError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+import {Url} from '../util/url';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  headers: Headers;
+  options: RequestOptions;
+
+  constructor(private http: Http) {
+    this.headers = new Headers(
+      {
+        'Content-Type': 'application/json',
+      });
+    this.options = new RequestOptions({headers: this.headers});
+  }
+
+  doLogin(loginCredentials): Observable<any> {
+    const headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'});
+    const options = new RequestOptions({method: RequestMethod.Post, headers: headers});
+    return this.http.post(Url.ADDRESS + Url.LOGIN, loginCredentials, options).pipe(
+      map((res: Response) => res.headers.get('Authorization').substring(7)));
+  }
+
+  doRegister(registerCredentials): Observable<any> {
+    return this.http.post(Url.ADDRESS + Url.REGISTER,
+      {
+        email: registerCredentials.email,
+        name: registerCredentials.name,
+        password: registerCredentials.password
+      },
+      this.options).pipe(
+      map((res: Response) => res.json().body),
+      catchError((error: any) => observableThrowError(error.json().error || 'Server error')));
+  }
+
+}
